0de6ce040da01e3c333563232a9c4ed66785ddf7
Yash_tambade <yashtambade56@gmail.com>
2026-01-10 12:51:42 +0530

    Add number classification program
    Implemented a program to check if a number is positive, negative, or zero.

---
+++ day 3/check_positive_negative_or_zero.cpp
@@ -0,0 +1,19 @@
+// write a program to check whether a number is positive, negative
+
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int num = 0;
+    if (num == 0) 
+        {cout << "this number is zoro" << endl;}
+    
+    else if 
+        (num % 2==0){
+        cout << "this number is positive" << endl;}
+    
+    else
+        {cout << "this number is negetiove" << endl;}
+
+    return 0;
+}
