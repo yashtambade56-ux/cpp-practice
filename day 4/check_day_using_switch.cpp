d666c0bcd5481142249a40a17eb5891646986edb
Yash_tambade <yashtambade56@gmail.com>
2026-01-10 12:53:30 +0530

    Create check_day_using_switch.cpp

---
+++ day 4/check_day_using_switch.cpp
@@ -0,0 +1,33 @@
+/ write the program whether to check day using switch case
+
+#include <iostream>
+using namespace std;
+int main() {
+    int day = 3;
+    switch (day) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid day" << endl;
+    }
+    return 0;
+}
