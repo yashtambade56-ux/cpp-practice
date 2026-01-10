de6e3df8af2746f2bb12f5574277874fe29e4497
Yash_tambade <yashtambade56@gmail.com>
2026-01-10 12:54:45 +0530

    Add reverse_numbers_5_to_1.cpp
    Implement a program to reverse numbers from 5 to 1.

---
+++ day 5/reverse_numbers_5_to_1.cpp
@@ -0,0 +1,14 @@
+// revers the number 5-1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 5;   // start from 4
+    do {
+        cout << i << "\n";
+        i--;     // decrease
+    } while (i >= 0);
+
+    return 0;
+}
