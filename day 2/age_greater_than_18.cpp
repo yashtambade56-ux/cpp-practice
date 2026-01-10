e1feed6b84a805c0e705c88784779f71686c9f2f
Yash_tambade <yashtambade56@gmail.com>
2026-01-10 12:45:40 +0530

    Add C++ program to check if age is greater than 18

---
+++ day 2/age_greater_than_18.cpp
@@ -0,0 +1,19 @@
+//c++ program to find age of a person is greater
+//than 18 or not
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age = 19;
+    if (age > 18)
+    {
+        cout << "Age is greater than 18";
+    }
+    else
+    {
+        cout << "Age is not greater than 18";
+    }
+    return 0;
+}
