396cc00912790978415d4c750c2217379568928f
Yash_tambade <yashtambade56@gmail.com>
2026-01-10 12:48:06 +0530

    Create check_positive_or_non_positive.cpp

---
+++ day 2/check_positive_or_non_positive.cpp
@@ -0,0 +1,21 @@
+// C++ program to find if a number is positive or non-positive
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = -1;
+
+    // Using if-else to determine if the number is positive or non-positive
+    if (number > 0)
+    {
+        cout << "The number is positive" << endl;
+    }
+    else
+    {
+        cout << "The number is non-positive" << endl;
+    }
+
+    return 0;
+}
