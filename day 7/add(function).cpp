11450086edeea73b0982d2408fd1bcbe53f1a95a
Yash_tambade <yashtambade56@gmail.com>
2026-01-20 21:05:14 +0530

    Create add(function).cpp

---
+++ day 7/add(function).cpp
@@ -0,0 +1,14 @@
+// write a program in c++ to add two numbers
+
+#include <iostream>
+using namespace std;
+
+void add() {
+    int a=5, b=3;
+    cout << "sum" << a + b <<endl;
+}
+
+int main() {
+    add();
+    return 0;
+}
