888a271e22b22a614fc4bf23901ed5b4a192ade7
Yash_tambade <yashtambade56@gmail.com>
2026-01-21 19:04:50 +0530

    Create current-year-finder.cpp

---
+++ day 8/current-year-finder.cpp
@@ -0,0 +1,14 @@
+// write a program for print current year 
+
+#include<iostream>
+using namespace std;
+
+int getyear()
+{
+    return 2026;
+}
+
+int main(){
+    cout << "current year:" << getyear();
+    return 0;
+}
